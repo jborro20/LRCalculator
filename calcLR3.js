cb30d5f640e2f2216019f276869ef10548d48dfd
jborro20 <62814779+jborro20@users.noreply.github.com>
2020-04-22 14:44:24 -0400

    Update calcLR3.js

--- calcLR3.js
+++ calcLR3.js
@@ -9,7 +9,6 @@ btn.addEventListener('click', function (e) {
 	let transFeeYC = document.getElementById('transFeeYC').value;
 	let franFeeYC = document.getElementById('franFeeYC').value;
 	let insurEOFeeYC = document.getElementById('insurEOFeeYC').value;
-	let insurEOFeePercentYC = document.getElementById('insurEOFeePercentYC').value;
 
 	let commissionSplitLR = document.getElementById('commissionSplitLR').value;
 	let deskFeeLR = document.getElementById('deskFeeLR').value;
@@ -18,7 +17,6 @@ btn.addEventListener('click', function (e) {
 	let transFeeLR = document.getElementById('transFeeLR').value;
 	let franFeeLR = document.getElementById('franFeeLR').value;
 	let insurEOFeeLR = document.getElementById('insurEOFeeLR').value;
-	let insurEOFeePercentLR = document.getElementById('insurEOFeePercentLR').value;
 
 	// calculated variables
 	let newGrossComLYLR = Math.round(grossComLY/(commissionSplitYC/100));
@@ -28,24 +26,22 @@ btn.addEventListener('click', function (e) {
 	let totFranFeeYC = (franFeeYC/100)*grossComLY;
 	let totinsEOFeeYC = insurEOFeeYC*numTransLY*1;
 	let totinsEOFeeLR = insurEOFeeLR*numTransLYLR*1;
-	let totinsEOPercentFeeYC = (insurEOFeePercentYC/100)*grossComLY;
-	let totinsEOPercentFeeLR = (insurEOFeePercentLR/100)*newGrossComLYLR;
 	let totFeesYC = (deskFeeYC*12)+(techFeeYC*12)+(totTransFeeYC*1)+
-	(totFranFeeYC)+totinsEOFeeYC+totinsEOPercentFeeYC;
+	(totFranFeeYC)+totinsEOFeeYC*1;
 	let totFeesLR = (deskFeeLR*1)+(techFeeLR*12)+(franFeeLR*12)+
-	totinsEOFeeLR*1+totinsEOPercentFeeLR*1;
+	totinsEOFeeLR*1;
 	let netComYC = Math.round(grossComLY-totFeesYC);
-	let netComLR = Math.round(grossComSplitLR-totFeesLR+totTransFeeLR);
+	let netComLR = Math.round(newGrossComLYLR-totFeesLR);
 
 
 	document.getElementById("grossComLYLR").value = newGrossComLYLR;
 	document.getElementById("numTransLYLR").value = numTransLY;
 	totalFeesYC.innerHTML = 'Total Fees: $ '+ (totFeesYC);
-	totalFeesLR.innerHTML = 'Total Fees: $ '+ (totFeesLR)+'*';
+	totalFeesLR.innerHTML = 'Total Fees: $ '+ (totFeesLR)+'**';
 	summarytotal.innerHTML = 'Summary:';
 	otherIncome.innerHTML = 'Net Income With Your Company: $' + (netComYC);
 	larosaIncome.innerHTML = 'Net Income With La Rosa North Florida: $' + (netComLR);
 	incomeDifference.innerHTML = 'La Rosa Difference: $ ' + (netComLR-netComYC);
-	noteLR.innerHTML = '*Transaction Fee at La Rosa North Florida is paid by the customer'
+	noteLR.innerHTML = '**Transaction Fee at La Rosa North Florida is paid by the customer'
 	});
 
